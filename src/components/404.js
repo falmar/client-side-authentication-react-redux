f4a4f3e648a56727cb4079d24396be6eeadb19ea
Jason Williams <jasonwilliams2308@gmail.com>
2017-07-13 20:34:29 +0100

    error page and redux devtools integration

---
+++ src/components/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+  return (
+    <div className="container" >
+      <p id="errorPage">404 Error Page</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/app';
@@ -16,9 +16,16 @@ import Signin from '../src/components/auth/signin';
 import Signout from '../src/components/auth/signout';
 import Signup from '../src/components/auth/signup';
 import Feature from '../src/components/feature';
+import ErrorPage from '../src/components/404';
+
+
+//redux dev tools setup
+const enhancers = compose(
+	window.devToolsExtension ? window.devToolsExtension() : f => f 
+);
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
-const store = createStoreWithMiddleware(reducers);
+const store = createStoreWithMiddleware(reducers, enhancers);
 const token = localStorage.getItem('jwt_web_token');
 
 //if jwt token in localstorage the we presume user is authorized and dispatch an action.
@@ -27,6 +34,8 @@ if(token){
 		type: AUTH_USER
 	});
 }
+
+
 
 ReactDOM.render(
 	<Provider store={store}>
@@ -37,6 +46,7 @@ ReactDOM.render(
 				<Route path="/feature" component={AuthGuard(Feature)} />			
 				<Route path='/signout' component={props => <Signout lat={-34.397} lng ={150.644}  />} />
 				<Route path='/signup' component={Signup} />
+				<Route path='/*' component={ErrorPage} />
 			</Route>
 		</Router>
 	</Provider>
